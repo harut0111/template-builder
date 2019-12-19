f549659f9742e904ff8457088d54cd9bf3715ec5
Harutyun Muqoyan <hmuqoyan@gmail.com>
2019-12-19 18:54:43 +0400

    mch

--- src/core/Comparision/index.js
+++ src/core/Comparision/index.js
@@ -2,7 +2,6 @@ export const areEqual = (prevProps, nextProps) => {
   const prevVal = JSON.stringify(prevProps.elData);
   const nextVal = JSON.stringify(nextProps.elData);
 
-  console.log('object', prevVal,nextVal)
   return prevVal === nextVal;
 };
 
