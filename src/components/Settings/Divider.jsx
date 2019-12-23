b6ac302a600945021f50842bcde79a79520aac08
Harutyun Muqoyan <hmuqoyan@gmail.com>
2019-12-23 18:33:26 +0400

    fixed bug

--- src/components/Settings/Divider.jsx
+++ src/components/Settings/Divider.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import uuid from "uuid";
 
 import { BORDER_TYPE_LIST, getActiveEl } from "../Constants";
@@ -12,7 +12,16 @@ const Divider = () => {
   const [borderWidth, setBorderWidth] = useState("1");
   const [borderColor, setBorderColor] = useState("#000000");
 
+  const borderTypeRef = useRef(null);
+  const borderWidthRef = useRef(null);
+  const borderColorRef = useRef(null);
+
   const handleOnChange = ev => {
+    const borderType = borderTypeRef.current.value;
+    const borderWidth = borderWidthRef.current.value;
+    const borderColor = borderColorRef.current.value;
+
+    console.log("object", borderType, borderWidth, borderColor);
     // ev.preventDefault();
     const elements = [...layout.elements];
     elements.forEach((element, i) => {
@@ -38,7 +47,7 @@ const Divider = () => {
   }, [memoizedCallback]);
 
   const DD = getActiveEl(layout).elData;
-  console.log('DD', DD);
+  console.log("DD", DD);
 
   // const memoizedLocalStateUpdate = useCallback(() => {
   //   if (DD) {
@@ -56,31 +65,34 @@ const Divider = () => {
     <div className="Divider">
       <h3>DIVIDER{Math.random()}</h3>
       <form>
-      <select 
-        value={DD ? DD.borderType: "solid"} 
-        // onChange={e => setBorderType(e.target.value)}
-        onChange={handleOnChange}
+        <select
+          value={DD ? DD.borderType : "solid"}
+          // onChange={e => setBorderType(e.target.value)}
+          onChange={handleOnChange}
+          ref={borderTypeRef}
         >
-        {BORDER_TYPE_LIST.map(value => (
-          <option key={uuid.v4()} value={value}>
-            {value}
-          </option>
-        ))}
-      </select>
+          {BORDER_TYPE_LIST.map(value => (
+            <option key={uuid.v4()} value={value}>
+              {value}
+            </option>
+          ))}
+        </select>
         <div>
           <input
             type="number"
-            value={DD ? +DD.borderWidth: "1" }
+            value={DD ? +DD.borderWidth : "1"}
             // onChange={e => setBorderWidth(String(e.target.value))}
             onChange={handleOnChange}
+            ref={borderWidthRef}
           />
           <span>px</span>
         </div>
         <input
           type="color"
-          value={DD ? DD.borderColor: "#000000"  }
+          value={DD ? DD.borderColor : "#000000"}
           // onChange={e => setBorderColor(e.target.value)}
           onChange={handleOnChange}
+          ref={borderColorRef}
         />
       </form>
     </div>
