39818eb4497b02b5b388be7a1a47e42925574c2c
Harutyun Muqoyan <hmuqoyan@gmail.com>
2019-12-25 15:00:03 +0400

    some changes in 'Text' component

--- src/components/Dashboard/TextData.jsx
+++ src/components/Dashboard/TextData.jsx
@@ -2,16 +2,25 @@ import React from "react";
 import { areEqual } from "../../core/Comparision";
 
 const TextData = ({ elData, active }) => {
-  return (
-    <div
-      className={`element ${active ? "element-active" : ""}`}
-    //   className="textData"
-    >
+  if (elData) {
+    return (
       <div
-        dangerouslySetInnerHTML={{ __html: elData && elData.toString("html") }}
-      />
-    </div>
-  );
+        className={`element ${active ? "element-active" : ""}`}
+        //   className="textData"
+      >
+        {elData.toString("html") !== "<p><br></p>" ? (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: elData && elData.toString("html")
+            }}
+          />
+        ) : (
+          "type some text"
+        )}
+      </div>
+    );
+  }
+  return null;
 };
 
 export default React.memo(TextData, (p, n) => areEqual(p, n));
