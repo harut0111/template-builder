9bbaf7854dd0403df516da46edc11c27ac3cab93
Harutyun Muqoyan <hmuqoyan@gmail.com>
2019-12-26 16:04:54 +0400

    bug fixed

--- src/components/Settings/SocialMedia.jsx
+++ src/components/Settings/SocialMedia.jsx
@@ -15,7 +15,6 @@ const SocialMedia = i => {
   console.log("SMD", SMD);
 
   const handleOnAdd = () => {
-    
     const socialMedia = socialMediaRef.current.value;
     const url = urlRef.current.value;
 
@@ -23,7 +22,7 @@ const SocialMedia = i => {
     elements.forEach((element, i) => {
       if (element.elId === layout.activeEl.id) {
         if (SMD) {
-          elements[i].elData = [...SMD, { socialMedia: 'Facebook', url: '' }];
+          elements[i].elData = [...SMD, { socialMedia: "Facebook", url: "" }];
         } else {
           elements[i].elData = [{ socialMedia, url }];
         }
@@ -32,8 +31,20 @@ const SocialMedia = i => {
     dispatch({ type: UPDATE_ELEMENT, payload: elements });
   };
 
-  useEffect(handleOnAdd , []);
+  useEffect(() => {
+    if(!SMD) {
+      const elements = [...layout.elements];
+  
+      elements.forEach((element, i) => {
+        if (element.elId === layout.activeEl.id) {
+            elements[i].elData = [{ socialMedia: 'Facebook', url: '' }];
+        }
+      });
+      dispatch({ type: UPDATE_ELEMENT, payload: elements });
 
+    }
+
+  } , []);
 
   const handleOnRemove = index => {
     if (SMD.length > 1) {
@@ -57,13 +68,12 @@ const SocialMedia = i => {
     elements.forEach((element, i) => {
       if (element.elId === layout.activeEl.id) {
         const CSMD = [...SMD];
-        CSMD[index] = {socialMedia, url}
+        CSMD[index] = { socialMedia, url };
         elements[i].elData = CSMD;
       }
     });
     dispatch({ type: UPDATE_ELEMENT, payload: elements });
   };
-
 
   const socialMediaIcons = [
     <FaFacebook />,
@@ -75,32 +85,32 @@ const SocialMedia = i => {
   return (
     <div className="socialMedia">
       <h3>Social Media</h3>
-      {(SMD || [{ socialMedia: "Facebook", url: "" }]).map((el, i) => (
-        <div className="socialMedia-main" key={i}>
-          {
-            socialMediaIcons[
-              SOCIAL_MEDIA_LIST.indexOf(el.socialMedia)
-            ]
-          }
-          <form onChange={ev => handleOnChange(ev, i)}>
-            <select defaultValue={el.socialMedia} ref={socialMediaRef}>
-              {SOCIAL_MEDIA_LIST.map((item, i) => (
-                <option key={i} value={item}>
-                  {item}
-                </option>
-              ))}
-            </select>
-            <input placeholder="URL" ref={urlRef} defaultValue={el.url} />
-            <hr />
-            <input
-              type="button"
-              value="Delete"
-              onClick={() => handleOnRemove(i)}
-            />
-          </form>
-          <hr />
-        </div>
-      ))}
+      {console.log(SMD)}
+      {SMD
+        ? SMD.map((el, i) => (
+            <div className="socialMedia-main" key={i}>
+              {socialMediaIcons[SOCIAL_MEDIA_LIST.indexOf(el.socialMedia)]}
+              <form onChange={ev => handleOnChange(ev, i)}>
+                <select defaultValue={el.socialMedia} ref={socialMediaRef}>
+                  {SOCIAL_MEDIA_LIST.map((item, i) => (
+                    <option key={i} value={item}>
+                      {item}
+                    </option>
+                  ))}
+                </select>
+                <input placeholder="URL" ref={urlRef} defaultValue={el.url} />
+                <hr />
+                <input
+                  type="button"
+                  value="Delete"
+                  onClick={() => handleOnRemove(i)}
+                />
+              </form>
+              <hr />
+            </div>
+          ))
+        : null}
+
       <button onClick={handleOnAdd}>Add another service</button>
     </div>
   );
