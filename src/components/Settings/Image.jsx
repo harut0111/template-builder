201b3170eda5efd8e8b6ff3e8afab7c5a4ab4b25
Harutyun Muqoyan <hmuqoyan@gmail.com>
2020-01-14 18:07:45 +0400

    added image uploading and rotation

--- src/components/Settings/Image.jsx
+++ src/components/Settings/Image.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 
 const Image = () => {
-    return (
-        <div className='Image'>
-            Image element
-        </div>
-    )
-}
+  const [imgSrc, setImgSrc] = useState(null);
+  const [deg, setDeg] = useState(0);
 
-export default Image
+  const inputRef = useRef(null);
+
+  const handleOnChanage = () => {
+    const reader = new FileReader();
+
+    const file = inputRef.current.files[0];
+    reader.readAsDataURL(file);
+    reader.onloadend = () => setImgSrc(reader.result);
+  };
+
+  return (
+    <div className="Image">
+      <input type="file" ref={inputRef} onChange={handleOnChanage} />
+      <div>
+        <button onClick={() => setDeg(deg - 90)}>Left</button>
+        <button onClick={() => setDeg(deg + 90)}>Right</button>
+      </div>
+      <div>
+        <img
+          src={imgSrc}
+          width="150"
+          height="150"
+          alt="img"
+          style={{ transform: `rotate(${deg}deg)` }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Image;
