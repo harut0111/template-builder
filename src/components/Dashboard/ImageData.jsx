f6c611bb3249c7f1a59b638210999285da71276e
Harutyun Muqoyan <hmuqoyan@gmail.com>
2020-01-14 19:24:23 +0400

    minor changes

--- src/components/Dashboard/ImageData.jsx
+++ src/components/Dashboard/ImageData.jsx
@@ -3,15 +3,20 @@ import { areEqual } from "../../core/Comparision";
 
 const ImageData = ({ elData, active }) => {
   return (
-    <div
-      className={`element ${active ? "element-active" : ""}`}
-    //   className="imageData"
-    >
-      <p>Image Data</p>
-      <div>{elData}</div>
+    <div className={`imageData element ${active ? "element-active" : ""}`}>
+      <div>
+        {elData ? (
+          <img
+            src={elData.imgSrc}
+            width="150"
+            height="150"
+            alt="img"
+            style={{ transform: `rotate(45deg)` }}
+          />
+        ) : null}
+      </div>
     </div>
   );
 };
 
 export default React.memo(ImageData, (p, n) => areEqual(p, n));
-
--- src/components/Settings/Image.jsx
+++ src/components/Settings/Image.jsx
@@ -1,19 +1,37 @@
 import React, { useRef, useState } from "react";
+import { useStateValue } from "../../context";
+import { UPDATE_ELEMENT } from "../../context/actions";
+import { getActiveEl } from "../Constants";
 
 const Image = () => {
-  const [imgSrc, setImgSrc] = useState(null);
+  const [{ layout }, dispatch] = useStateValue();
+
+  // const [imgSrc, setImgSrc] = useState(null);
   const [deg, setDeg] = useState(0);
 
   const inputRef = useRef(null);
+
+  const ID = getActiveEl(layout).elData;
 
   const handleOnChanage = () => {
     const reader = new FileReader();
 
     const file = inputRef.current.files[0];
     reader.readAsDataURL(file);
-    reader.onloadend = () => setImgSrc(reader.result);
+    reader.onloadend = () => {
+      const elements = [...layout.elements];
+      elements.forEach((element, i) => {
+        if (element.elId === layout.activeEl.id) {
+          elements[i].elData = {
+            imgSrc: reader.result
+          };
+        }
+      });
+      dispatch({ type: UPDATE_ELEMENT, payload: elements });
+    };
   };
 
+  console.log("ID", ID);
   return (
     <div className="Image">
       <input type="file" ref={inputRef} onChange={handleOnChanage} />
@@ -22,13 +40,15 @@ const Image = () => {
         <button onClick={() => setDeg(deg + 90)}>Right</button>
       </div>
       <div>
-        <img
-          src={imgSrc}
-          width="150"
-          height="150"
-          alt="img"
-          style={{ transform: `rotate(${deg}deg)` }}
-        />
+        {ID ? (
+          <img
+            src={ID.imgSrc}
+            width="150"
+            height="150"
+            alt="img"
+            style={{ transform: `rotate(${deg}deg)` }}
+          />
+        ) : null}
       </div>
     </div>
   );
