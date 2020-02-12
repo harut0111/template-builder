aaec3f8b79d86e39da3b952f065d129e61c4ace6
Harutyun Muqoyan <hmuqoyan@gmail.com>
2020-02-12 15:22:20 +0400

    cleaned up 'Map.jsx' component

--- src/components/Settings/Map.jsx
+++ src/components/Settings/Map.jsx
@@ -2,9 +2,12 @@ import React, { useEffect } from "react";
 import Slider from "./_Slider";
 import { useStateValue } from "../../context";
 import { UPDATE_ELEMENT } from "../../context/actions";
+import { updateElementData } from "../Constants";
 
 const Map = () => {
   const [{ layout }, dispatch] = useStateValue();
+  const els = layout.elements;
+  const activeElId = layout.activeEl.id;
 
   const addressRef = React.useRef(null);
   const sliderRef = React.useRef(null);
@@ -14,14 +17,9 @@ const Map = () => {
     const zoom = sliderRef.current.state.value;
     const address = addressRef.current.value;
 
-    const elements = [...layout.elements];
-    elements.forEach((element, i) => {
-      if (element.elId === layout.activeEl.id) {
-        elements[i].elData = {
-          address,
-          zoom
-        };
-      }
+    const elements = updateElementData(els, activeElId, {
+      address,
+      zoom
     });
     dispatch({ type: UPDATE_ELEMENT, payload: elements });
   };
