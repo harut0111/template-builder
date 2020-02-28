595d328817b7fed98095723177eb86af906924f2
Harut Muqoyan <hmuqoyan@gmail.com>
2020-02-28 18:21:41 +0400

    bug fixed and cleaned up the code

--- src/components/Settings/Text.jsx
+++ src/components/Settings/Text.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import { useStateValue } from "../../context";
 import { UPDATE_ELEMENT } from "../../context/actions";
 import { updateElementData } from "../../utils/updateElData";
-import { convertToRaw } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import draftToHtml from "draftjs-to-html";
 import { htmlToDraftJs } from "../../utils/htmlToDraftJs";
+import { convertToRaw } from "draft-js";
+import { getActiveEl } from "../../utils/getActiveEl";
+import draftToHtml from "draftjs-to-html";
 
 const Text = () => {
   const [{ layout }, dispatch] = useStateValue();
   const els = layout.elements;
   const activeElId = layout.activeEl.id;
+
+  const TD = getActiveEl(layout).elData;
 
   const handleOnChange = editorState => {
     const markup = draftToHtml(convertToRaw(editorState.getCurrentContent()));
@@ -24,7 +27,7 @@ const Text = () => {
   return (
     <div>
       <Editor
-        defaultEditorState={htmlToDraftJs(localStorage.content)}
+        defaultEditorState={htmlToDraftJs(TD ? TD.markup : "<p></p>")}
         wrapperClassName="demo-wrapper"
         editorClassName="demo-editor"
         onEditorStateChange={handleOnChange}
--- src/configs/constants/index.js
+++ src/configs/constants/index.js
@@ -28,61 +28,23 @@ import SocialMediaData from "../../components/Dashboard/SocialMediaData";
 import AudioData from "../../components/Dashboard/AudioData";
 import LinkButtonData from "../../components/Dashboard/LinkButtonData";
 
-export const EL_LIST = [
-  { label: "Text", Icon: MdTextFields },
-  { label: "Video", Icon: FaRegPlayCircle },
-  { label: "Image", Icon: FaImage },
-  { label: "Slider", Icon: IoIosImages },
-  { label: "Social Media", Icon: IoMdShare },
-  { label: "Audio", Icon: FiHeadphones },
-  { label: "Map", Icon: FiMapPin },
-  { label: "Link/Button", Icon: FiLink2 },
-  { label: "Divider", Icon: TiDivideOutline }
-];
-
-export const EL_SETS_LIST = [
-  { label: "Text", Sets: Text },
-  { label: "Video", Sets: Video },
-  { label: "Image", Sets: Image },
-  { label: "Slider", Sets: Slider },
-  { label: "Social Media", Sets: SocialMedia },
-  { label: "Audio", Sets: Audio },
-  { label: "Map", Sets: Map },
-  { label: "Link/Button", Sets: LinkButton },
-  { label: "Divider", Sets: Divider }
-];
-
-export const EL_DATA_LIST = [
-  { label: "Text", Data: TextData, id: uuid.v4() },
-  { label: "Video", Data: VideoData, id: uuid.v4() },
-  { label: "Image", Data: ImageData, id: uuid.v4() },
-  { label: "Slider", Data: SliderData, id: uuid.v4() },
-  { label: "Social Media", Data: SocialMediaData, id: uuid.v4() },
-  { label: "Audio", Data: AudioData, id: uuid.v4() },
-  { label: "Map", Data: MapData, id: uuid.v4() },
-  { label: "Link/Button", Data: LinkButtonData, id: uuid.v4() },
-  { label: "Divider", Data: DividerData, id: uuid.v4() }
-];
+const LABELS = ["Text", "Video",  "Image", "Slider", "Social Media", "Audio", "Map", "Link/Button", "Divider"];
+const ICONS = [MdTextFields, FaRegPlayCircle, FaImage, IoIosImages, IoMdShare, FiHeadphones, FiMapPin, FiLink2, TiDivideOutline];
+const SETS = [Text, Video, Image, Slider, SocialMedia, Audio, Map, LinkButton, Divider];
+const DATA = [TextData, VideoData, ImageData, SliderData, SocialMediaData, AudioData, MapData, LinkButtonData, DividerData];
+const VIDEO_PROVIDERS = ["YouTube", "Facebook"];
+const SOCIAL_MEDIAS = ["Facebook", "Twitter", "Instagram", "YouTube"];
+const SOCIAL_MEDIA_ICONS = [FaFacebook, FaTwitter, FaInstagram, FaYoutube];
 
 export const BORDER_TYPE_LIST = ["solid", "dotted", "dashed", "double", "none"];
-
 export const BAR_LIST = ["Elements", "Component Settings"];
 
-export const VIDEO_PROVIDER_LIST = [
-  { name: "YouTube", id: uuid.v4() },
-  { name: "Facebook", id: uuid.v4() }
-  // {name: "Vimeo", id: uuid.v4()},
-  // {name: "Metacafe", id: uuid.v4()},
-  // {name: "Veoh", id: uuid.v4()}
-];
-
+export const EL_LIST = LABELS.map((label, i) => ({label: label, Icon: ICONS[i]}));
+export const EL_SETS_LIST = LABELS.map((label, i) => ({label: label, Sets: SETS[i]}));
+export const EL_DATA_LIST = LABELS.map((label, i) => ({label: label, Data: DATA[i], id: uuid.v4()}));
+export const VIDEO_PROVIDER_LIST = VIDEO_PROVIDERS.map((provider) => ({name: provider, id: uuid.v4()}))
 export const AUDIO_PROVIDER_LIST = ["Soundcloud"];
-export const SOCIAL_MEDIA_LIST = [
-  { label: "Facebook", Icon: FaFacebook, id: uuid.v4() },
-  { label: "Twitter", Icon: FaTwitter, id: uuid.v4() },
-  { label: "Instagram", Icon: FaInstagram, id: uuid.v4() },
-  { label: "YouTube", Icon: FaYoutube, id: uuid.v4() }
-];
+export const SOCIAL_MEDIA_LIST = SOCIAL_MEDIAS.map((label, i) => ({label: label, Icon: SOCIAL_MEDIA_ICONS[i], id: uuid.v4()}))
 
 export const FORMAT_LIST = [
   {
@@ -108,8 +70,56 @@ export const FORMAT_LIST = [
   }
 ];
 
-export const LOGIN_PATH = "/login";
-export const MAIN_PATH = "/main";
-export const IS_LOGGED_IN = "isLoggedIn"; // temp
-// export const HISTORY_TABLE_HEADERS = [];
-// export const NAVBAR_LIST = ["Currency Rates", "Converter", "History"];
+
+// export const EL_LIST = [
+//   { label: "Text", Icon: MdTextFields },
+//   { label: "Video", Icon: FaRegPlayCircle },
+//   { label: "Image", Icon: FaImage },
+//   { label: "Slider", Icon: IoIosImages },
+//   { label: "Social Media", Icon: IoMdShare },
+//   { label: "Audio", Icon: FiHeadphones },
+//   { label: "Map", Icon: FiMapPin },
+//   { label: "Link/Button", Icon: FiLink2 },
+//   { label: "Divider", Icon: TiDivideOutline }
+// ];
+
+// export const EL_SETS_LIST = [
+//   { label: "Text", Sets: Text },
+//   { label: "Video", Sets: Video },
+//   { label: "Image", Sets: Image },
+//   { label: "Slider", Sets: Slider },
+//   { label: "Social Media", Sets: SocialMedia },
+//   { label: "Audio", Sets: Audio },
+//   { label: "Map", Sets: Map },
+//   { label: "Link/Button", Sets: LinkButton },
+//   { label: "Divider", Sets: Divider }
+// ];
+
+// export const EL_DATA_LIST = [
+//   { label: "Text", Data: TextData, id: uuid.v4() },
+//   { label: "Video", Data: VideoData, id: uuid.v4() },
+//   { label: "Image", Data: ImageData, id: uuid.v4() },
+//   { label: "Slider", Data: SliderData, id: uuid.v4() },
+//   { label: "Social Media", Data: SocialMediaData, id: uuid.v4() },
+//   { label: "Audio", Data: AudioData, id: uuid.v4() },
+//   { label: "Map", Data: MapData, id: uuid.v4() },
+//   { label: "Link/Button", Data: LinkButtonData, id: uuid.v4() },
+//   { label: "Divider", Data: DividerData, id: uuid.v4() }
+// ];
+
+// export const VIDEO_PROVIDER_LIST = [
+//   { name: "YouTube", id: uuid.v4() },
+//   { name: "Facebook", id: uuid.v4() }
+//   // {name: "Vimeo", id: uuid.v4()},
+//   // {name: "Metacafe", id: uuid.v4()},
+//   // {name: "Veoh", id: uuid.v4()}
+// ];
+
+
+
+// export const SOCIAL_MEDIA_LIST = [
+//   { label: "Facebook", Icon: FaFacebook, id: uuid.v4() },
+//   { label: "Twitter", Icon: FaTwitter, id: uuid.v4() },
+//   { label: "Instagram", Icon: FaInstagram, id: uuid.v4() },
+//   { label: "YouTube", Icon: FaYoutube, id: uuid.v4() }
+// ];
